e3cfad6ef05124d5af5c9be4fb805d5f5672d24c
vijay0405 <uvijaysimha@gmail.com>
2020-05-30 23:18:30 +0530

    added component and routing module

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { RegistrationsComponent } from './registrations/registrations.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegistrationsComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
