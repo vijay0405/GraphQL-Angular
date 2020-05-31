addc05682dcf1f860f86f2098dd957a7bb91feb2
vijay0405 <uvijaysimha@gmail.com>
2020-06-01 00:00:54 +0530

    udated content

--- resolver.js
+++ resolver.js
@@ -32,29 +32,29 @@ const GQDate = new GraphQLScalarType({
 const registrations = [
   {
     id: 1,
-    firstName: "Johan",
-    lastName: "Peter",
+    firstName: "user",
+    lastName: "1",
     dob: new Date("2014-08-31"),
-    email: "johan@gmail.com",
-    password: "johan123",
-    country: "UK"
+    email: "user1@gmail.com",
+    password: "user1",
+    country: "india"
   },
   {
     id: 2,
-    firstName: "Mohamed",
-    lastName: "Tariq",
+    firstName: "user",
+    lastName: "2",
     dob: new Date("1981-11-24"),
-    email: "tariq@gmail.com",
-    password: "tariq123",
-    country: "UAE"
+    email: "user2@gmail.com",
+    password: "user2",
+    country: "india"
   },
   {
     id: 3,
-    firstName: "Nirmal",
-    lastName: "Kumar",
+    firstName: "user",
+    lastName: "3",
     dob: new Date("1991-09-02"),
-    email: "nirmal@gmail.com",
-    password: "nirmal123",
+    email: "user3@gmail.com",
+    password: "user3",
     country: "India"
   }
 ];
